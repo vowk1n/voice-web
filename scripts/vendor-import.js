f08203229f9e0f5161bac69d1048237c5b06379c
Gregor Weber <gregorwbr@gmail.com>
2019-05-15 12:53:39 +0200

    throttle S3 upload

--- scripts/vendor-import.js
+++ scripts/vendor-import.js
@@ -10,59 +10,71 @@ const uuid = require('uuid/v4');
 // surround a string in quotes
 const q = str => "'" + str + "'";
 
-const options = commandLineArgs([
-  {
-    name: 'src',
-    description: 'xlsx file one clip per row',
-    type: String,
-    defaultOption: true,
-  },
-]);
+async function run() {
+  const options = commandLineArgs([
+    {
+      name: 'src',
+      description: 'xlsx file one clip per row',
+      type: String,
+      defaultOption: true,
+    },
+  ]);
 
-const s3 = new AWS.S3({ params: { Bucket: 'cv-mandarin' } });
-const lines = parse(fs.readFileSync(options.src, 'utf-8'), {
-  from_line: 2,
-  delimiter: ';',
-});
-
-const statements = [
-  "SET @locale_id = (SELECT id FROM locales WHERE name = 'zh-CN')",
-];
-for (const [id, , , sentence, url] of lines) {
-  const pass = new PassThrough();
-  https.get(url, { rejectUnauthorized: false }, response => {
-    response.pipe(pass);
-  });
-  const fileName = id + '.mp3';
-  s3.upload({ Key: fileName, Body: pass }).send(err => {
-    if (err) {
-      console.error(err);
-      process.exit();
-    }
+  const s3 = new AWS.S3({ params: { Bucket: 'cv-mandarin' } });
+  const lines = parse(fs.readFileSync(options.src, 'utf-8'), {
+    from_line: 2,
+    delimiter: ';',
   });
 
-  statements.push(
-    'INSERT INTO user_clients (client_id) VALUES (' + q(id) + ')'
-  );
-
-  const sentenceId = uuid();
-  const sentenceData = [q(sentenceId), q(sentence), 'TRUE', '@locale_id'];
-  statements.push(
-    'INSERT INTO sentences (id, text, is_used, locale_id) ' +
-      `VALUES (${sentenceData.join(', ')})`
-  );
-
-  const clipData = [
-    q(id),
-    q(fileName),
-    q(sentenceId),
-    q(sentence),
-    '@locale_id',
+  const statements = [
+    "SET @locale_id = (SELECT id FROM locales WHERE name = 'zh-CN')",
   ];
-  statements.push(
-    'INSERT INTO clips (client_id, path, original_sentence_id, sentence, locale_id) ' +
-      `VALUES (${clipData.join(', ')})`
-  );
+
+  let uploads = 0;
+  for (const [id, , , sentence, url] of lines) {
+    if (uploads > 250) {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    const pass = new PassThrough();
+    https.get(url, { rejectUnauthorized: false }, response => {
+      response.pipe(pass);
+    });
+    const fileName = id + '.mp3';
+    s3.upload({ Key: fileName, Body: pass }).send(err => {
+      uploads--;
+      if (err) {
+        console.error(err);
+        process.exit();
+      }
+    });
+    uploads++;
+
+    statements.push(
+      'INSERT IGNORE INTO user_clients (client_id) VALUES (' + q(id) + ')'
+    );
+
+    const sentenceId = uuid();
+    const sentenceData = [q(sentenceId), q(sentence), 'TRUE', '@locale_id'];
+    statements.push(
+      'INSERT INTO sentences (id, text, is_used, locale_id) ' +
+        `VALUES (${sentenceData.join(', ')})`
+    );
+
+    const clipData = [
+      q(id),
+      q(fileName),
+      q(sentenceId),
+      q(sentence),
+      '@locale_id',
+    ];
+    statements.push(
+      'INSERT INTO clips (client_id, path, original_sentence_id, sentence, locale_id) ' +
+        `VALUES (${clipData.join(', ')})`
+    );
+  }
+
+  console.log(statements.map(s => s + ';').join('\n'));
 }
 
-console.log(statements.map(s => s + ';').join('\n'));
+run().catch(e => console.error(e));
